af13390ec0a69de86527c8b7410e56fa6e561f90
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2023-12-23 15:52:01 -0600

    reused code

--- day13/day13.go
+++ day13/day13.go
@@ -15,16 +15,15 @@ func Day13() {
 	fmt.Println("Part2:", part2Answer)
 }
 
-func getPart1Answer(lines []string) int {
-	patterns := getPatterns(lines)
+func getPart1Answer(patterns [][]string) int {
 	result := 0
 	for _, pattern := range patterns {
-		horizontal := getHorizontalMirror(pattern)
+		horizontal := getHorizontalMirror(pattern, 0)
 		if horizontal != 0 {
 			result += 100 * horizontal
 			continue
 		}
-		vertical := getVerticalMirror(pattern)
+		vertical := getVerticalMirror(pattern, 0)
 		result += vertical
 	}
 	return result
@@ -42,27 +41,9 @@ func getTranspose(mat []string) []string {
 	return transpose
 }
 
-func getVerticalMirror(pattern []string) int {
+func getVerticalMirror(pattern []string, allowance int) int {
 	transpose := getTranspose(pattern)
-	return getHorizontalMirror(transpose)
-}
-
-func getHorizontalMirror(pattern []string) int {
-	for i := 1; i < len(pattern); i++ {
-		if checkMirror(pattern, i) {
-			return i
-		}
-	}
-	return 0
-}
-
-func checkMirror(pattern []string, position int) bool {
-	for i, j := position, position-1; i < len(pattern) && j >= 0; i, j = i+1, j-1 {
-		if pattern[i] != pattern[j] {
-			return false
-		}
-	}
-	return true
+	return getHorizontalMirror(transpose, allowance)
 }
 
 func getPatterns(lines []string) [][]string {
@@ -79,8 +60,7 @@ func getPatterns(lines []string) [][]string {
 	return append(result, current)
 }
 
-func checkMirrorWithSumdge(pattern []string, position int) bool {
-	allowance := 1
+func checkMirror(pattern []string, position int, allowance int) bool {
 	for i, j := position, position-1; i < len(pattern) && j >= 0; i, j = i+1, j-1 {
 		for x := range pattern[i] {
 			if pattern[i][x] != pattern[j][x] {
@@ -96,41 +76,35 @@ func checkMirrorWithSumdge(pattern []string, position int) bool {
 
 }
 
-func getVerticalMirrorWithSmudge(pattern []string) int {
-	transpose := getTranspose(pattern)
-	return getHorizontalMirrorWithSmudge(transpose)
-}
-
-func getHorizontalMirrorWithSmudge(pattern []string) int {
+func getHorizontalMirror(pattern []string, allowance int) int {
 	for i := 1; i < len(pattern); i++ {
-		if checkMirrorWithSumdge(pattern, i) {
+		if checkMirror(pattern, i, allowance) {
 			return i
 		}
 	}
 	return 0
 }
 
-func getPart2Answer(lines []string) int {
-	patterns := getPatterns(lines)
+func getPart2Answer(patterns [][]string) int {
 	result := 0
 	for _, pattern := range patterns {
-		horizontal := getHorizontalMirrorWithSmudge(pattern)
+		horizontal := getHorizontalMirror(pattern, 1)
 		if horizontal != 0 {
 			result += 100 * horizontal
 			continue
 		}
-		vertical := getVerticalMirrorWithSmudge(pattern)
+		vertical := getVerticalMirror(pattern, 1)
 		result += vertical
 	}
 	return result
 }
 
-func getInput() []string {
+func getInput() [][]string {
 	lines := []string{}
 	file, err := os.Open("day13/input.txt")
 	if err != nil {
 		fmt.Println("error reading input")
-		return lines
+		return [][]string{}
 	}
 	defer file.Close()
 
@@ -139,5 +113,5 @@ func getInput() []string {
 		line := scanner.Text()
 		lines = append(lines, line)
 	}
-	return lines
+	return getPatterns(lines)
 }
